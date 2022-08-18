aa5237a4f85033dc88e3174a23e68837923a786a
Sukeb <sukebushido@gmail.com>
2022-08-18 12:35:42 +0200

    Add config files

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ export default defineConfig({
         laravel([
             'resources/css/app.css',
             'resources/js/app.js',
+            'resources/sass/app.scss'
         ]),
     ],
+    
 });
