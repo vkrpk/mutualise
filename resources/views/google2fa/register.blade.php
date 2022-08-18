f6b6e0c81874ad67edeb57cd38b61e0182e40f3d
vkrpk <victor.krupka@orange.fr>
2022-08-18 17:37:32 +0200

    Enlèvement du var_dump() dans le controller

--- resources/views/google2fa/register.blade.php
+++ resources/views/google2fa/register.blade.php
@@ -10,7 +10,6 @@
                 <div class="panel-body" style="text-align: center;">
                     <p>Set up your two factor authentication by scanning the barcode below. Alternatively, you can use the code {{ $secret }}</p>
                     <div>
-                        {{-- <iframe src="{{ $QR_Image }}" width="400px" height="400px"></iframe> --}}
                         {!! $QR_Image !!}
                     </div>
                     <p>You must set up your Google Authenticator app before continuing. You will be unable to login otherwise</p>
