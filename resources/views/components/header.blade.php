4807a05c591acdb11592154aebf9337428b63e91
Sukeb <sukebushido@gmail.com>
2022-08-19 11:36:34 +0200

    Résolution du pb qui faisait que c'était impossible de se déco + reroutage vers la route 'login' quand arrivée sur Page d'accueil

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -41,6 +41,9 @@
                                          document.getElementById('logout-form').submit();">
                                     Se déconnecter
                                 </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
                             </div>
                         </li>                
                     @endguest
