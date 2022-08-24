6e2bafaf5d78cabbf7ad2ed52ebf23ff26c6f8dc
vkrpk <victor.krupka@orange.fr>
2022-08-24 11:48:58 +0200

    Remove AuthController and old paths

--- routes/web.php
+++ routes/web.php
@@ -20,23 +20,10 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::middleware(['2fa'])->group(function () {
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'],)->name('home');
-    Route::post('/2fa', function () {
-        return redirect(route('home'));
-    })->name('2fa');
-});
-
-
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
 
-
-Route::get('/2fa/enable', 'Google2FAController@enableTwoFactor');
-Route::get('/2fa/disable', 'Google2FAController@disableTwoFactor');
-Route::get('/2fa/validate', 'AuthAuthController@getValidateToken');
-Route::post('/2fa/validate', ['middleware' => 'throttle:5', 'uses' => 'AuthAuthController@postValidateToken']);
 
 Route::get('/profil', [App\Http\Controllers\Auth\ProfilViewController::class, 'index'],)->name('profilView');
 Route::get('/test', [TestController::class, 'test'])->name('test');
