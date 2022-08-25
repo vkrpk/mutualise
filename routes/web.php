a73f2c8a408915408b8bbad94938e91a06887606
Sukeb <sukebushido@gmail.com>
2022-08-25 11:14:09 +0200

    Ajout du middleware auth pour les routes concernant le profil

--- routes/web.php
+++ routes/web.php
@@ -25,18 +25,20 @@ Route::group(['prefix' => 'admin'], function () {
 });
 
 
-Route::get('/profil', [App\Http\Controllers\Auth\ProfilViewController::class, 'index'])->name('profilIndex');
-Route::get('/billing', [App\Http\Controllers\Auth\ProfilViewController::class, 'billing'])->name('profilBilling');
-Route::get('/security', [App\Http\Controllers\Auth\ProfilViewController::class, 'security'])->name('profilSecurity');
-Route::get('/notifications', [App\Http\Controllers\Auth\ProfilViewController::class, 'notifications'])->name('profilNotifications');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/profil', [App\Http\Controllers\Auth\ProfilViewController::class, 'index'])->name('profilIndex');
+    Route::get('/billing', [App\Http\Controllers\Auth\ProfilViewController::class, 'billing'])->name('profilBilling');
+    Route::get('/security', [App\Http\Controllers\Auth\ProfilViewController::class, 'security'])->name('profilSecurity');
+    Route::get('/notifications', [App\Http\Controllers\Auth\ProfilViewController::class, 'notifications'])->name('profilNotifications');
+});
 
 Route::get('/test', [TestController::class, 'test'])->name('test');
 
-Route::group(['prefix'=>'2fa'], function(){
-    Route::get('/','App\Http\Controllers\LoginSecurityController@show2faForm');
-    Route::post('/generateSecret','App\Http\Controllers\LoginSecurityController@generate2faSecret')->name('generate2faSecret');
-    Route::post('/enable2fa','App\Http\Controllers\LoginSecurityController@enable2fa')->name('enable2fa');
-    Route::post('/disable2fa','App\Http\Controllers\LoginSecurityController@disable2fa')->name('disable2fa');
+Route::group(['prefix' => '2fa'], function () {
+    Route::get('/', 'App\Http\Controllers\LoginSecurityController@show2faForm');
+    Route::post('/generateSecret', 'App\Http\Controllers\LoginSecurityController@generate2faSecret')->name('generate2faSecret');
+    Route::post('/enable2fa', 'App\Http\Controllers\LoginSecurityController@enable2fa')->name('enable2fa');
+    Route::post('/disable2fa', 'App\Http\Controllers\LoginSecurityController@disable2fa')->name('disable2fa');
 
     // 2fa middleware
     Route::post('/2faVerify', function () {
@@ -48,4 +50,3 @@ Route::group(['prefix'=>'2fa'], function(){
 Route::get('/test_middleware', function () {
     return "2FA middleware work!";
 })->middleware(['auth', '2fa']);
-
