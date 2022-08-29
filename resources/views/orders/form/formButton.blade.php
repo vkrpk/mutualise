c477a0e781f7d0e9c0e590fe66d633cffaac77d5
vkrpk <victor.krupka@orange.fr>
2022-08-29 13:58:28 +0200

    Refactorisation de la table des prix et passage en mode grid dans le scss

---
+++ resources/views/orders/form/formButton.blade.php
@@ -0,0 +1,9 @@
+<form action="{{ url('member_accesses') }}" class="needs-validation" method="POST" novalidate id="formOrder">@csrf
+    <input type="hidden" id="form_level" name="form_level" value="BAS">
+    <input type="hidden" id="form_free_account" name="form_free_account" value="0">
+    <input type="hidden" id="form_diskspace" name="form_diskspace" value="10">
+    <input type="hidden" id="form_expire" name="form_expire" value="">
+    <button class="btn btn-lg fs-3 btn-primary" type="submit"><i class="fa-solid fa-cart-shopping"></i>
+        Ajouter au panier
+    </button>
+</form>
