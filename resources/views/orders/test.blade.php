f04076689b253139f35b314dd9d3f1d54d433c87
vkrpk <victor.krupka@orange.fr>
2022-08-30 15:27:07 +0200

    simplification de l'html dans la page test

--- resources/views/orders/test.blade.php
+++ resources/views/orders/test.blade.php
@@ -16,153 +16,104 @@
         break;
     }
 @endphp
-
-<div class="my-4 container-sm overflow-hidden p-0">
-    <div class="alert alert-primary mt-4 mb-2 fs-2 fw-bolder" role="alert">
-        <span>Ajouter un accès</span>
-    </div>
-    <div class="card border-secondary">
-        <div class="card-body d-flex flex-column align-middle">
-            {{-- INPUT --}}
-            @include('orders.form.boxFreeTrial')
+<div class="container-sm overflow-hidden p-0">
+    <div class="row mx-2 mx-sm-0 g-2 text-center m-3">
+        <div class="alert alert-primary fs-3 fw-bolder" role="alert">
+            <span>Ajouter un accès</span>
         </div>
+        <div class="card border-secondary">
+            <div class="card-body d-flex flex-column align-middle">
+                {{-- INPUT --}}
+                @include('orders.form.boxFreeTrial')
+                {{-- INPUT --}}
+            </div>
+        </div>
+        <h1 class="pricing headline text-secondary" style="font-family: Roboto, sans-serif;">Choisissez votre formule</h1>
+        <h3 class="pricing-sub-headline">Toutes vos données fragmentées, copiées et réparties sur différents serveurs et Datacenters selon le niveau de disponibilité choisi.
+            <a class="text-primary" href="https://www.dedikam.com/lexique/#niveaux" target="_blank">En savoir plus</a>
+        </h3>
     </div>
-    <h1 class="pricing headline text-center mb-4" style="font-family: Roboto, sans-serif;">
-        Choisissez votre formule</h1>
-    <h3 class="pricing-sub-headline text-center">Toutes vos données fragmentées, copiées et réparties sur différents serveurs et Datacenters selon le niveau de disponibilité choisi.
-        <a class="text-secondary" href="https://www.dedikam.com/lexique/#niveaux" target="_blank">En savoir plus</a>
-    </h3>
-    <div class="comparison">
+    {{-- TABLEAU COMPARATIF --}}
+    <div class="comparison mb-4">
         @include('orders.form.pricingTable')
     </div>
-    <div class="row mt-4 espace">
-        <div class="col">
-            <div class="row">
-                <div class="col d-flex flex-nowrap justify-content-center flex-column">
-                    <!--<span class="offer_legend1">2</span>-->
-                    <p class="align-self-center">Choisissez votre espace disque en déplaçant le
-                        curseur orange vers la droite</p>
-                    <p class="align-self-center">Tarif affiché en bas de page</p>
-                </div>
-            </div>
-            <div class="row pt-2 offer_content">
-                <div class="col">
-                    <div class="col">
-                        <div class="col d-none d-md-grid col-md-1"></div>
-                        <div class="col align-self-center m-4">
-                            <div class="row range-slider-container align-items-center">
-                                <div class="col col-2"><span>Espace disque : <span id="taille_output">10
-                                            Go</span></span></div>
-
-                                <input type="range" class="rs-range flex-grow-1" name="taille" id="taille" min="10"
-                                    max="5000" step="10" value="10" form="formOrder">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col text-center"><span>Trafic illimité - Bande passante : 500 Mbit/s à
-                            1Gbit/s<br>Au dessus de 5000Go, veuillez nous&nbsp;<a class="text-secondary"
-                                href="https://www.dedikam.com/contact/" target="_blank">contacter</a>.<br><br></span>
-                    </div>
-                </div>
+    {{-- TABLEAU COMPARATIF --}}
+    <div class="row mx-2 mx-sm-0 g-2 m-3">
+        <p class="p-2 p-sm-4 rounded-3 fs-5 bg-white border">Choisissez votre espace disque en déplaçant le curseur orange vers la droite, tarif affiché en bas de page.</p>
+        <div class="mx-4 my-4 range-slider-container text-center">
+            <span>Espace disque : <span id="taille_output">10Go</span></span>
+            <input type="range" class="rs-range flex-grow-1" name="taille" id="taille" min="10" max="5000" step="10" value="10" form="formOrder">
+        </div>
+        <div class="text-center">
+            <span>Trafic illimité - Bande passante : 500 Mbit/s à 1Gbit/s
+                <br>Au dessus de 5 000Go, veuillez nous
+                <a class="text-primary" href="https://www.dedikam.com/contact/" target="_blank">contacter</a>.
+            </span>
+        </div>
+        <span class="offer_legend2 align-self-left mt-4">Choisissez un nom pour l'accès<br></span>
+        <div class="form-floating offer_content pb-2 mt-0">
+            <input type="text" name="form_access_name" data-bs-toggle="tooltip" data-bss-tooltip="" data-bs-placement="right" id="access_name" class="form-control  @error('form_access_name') is-invalid @enderror form=" formOrder"" @error('form_access_name') autofocus @enderror placeholder="Nom de l'accès" required style="width: 20rem;" title="Donnée obligatoire" maxlength="40" value="{{ old('form_access_name') }}" aria-describedby="validationform_access_nameFeedback">
+            <label class="form-label" for="access_name">Nom de l'accès
+                <sup>
+                    <i class="fa-solid fa-asterisk" style="font-size: 8px;color: red;margin-top: -14px;"></i>
+                </sup>
+            </label>
+            <div class="invalid-feedback">
+                Le nom de l'accès est obligatoire
             </div>
         </div>
-    </div>
-    <div class="row mt-4">
-        <div class="col">
-            <div class="row">
-                <div class="col d-flex flex-nowrap">
-                    <!--<span class="offer_legend1">3</span>-->
-                    <span class="offer_legend2 align-self-center">Choisissez un nom pour l'accès<br></span>
-                </div>
-            </div>
-            <div class="row pt-2 offer_content pb-2">
-                <div class="col">
-                    <div class="form-floating">
-                        <input type="text" name="form_access_name" data-bs-toggle="tooltip" data-bss-tooltip=""
-                            data-bs-placement="right" id="access_name"
-                            class="form-control  @error('form_access_name') is-invalid @enderror form=" formOrder""
-                            @error('form_access_name') autofocus @enderror placeholder="Nom de l'accès" required
-                            style="width: 20rem;" title="Donnée obligatoire" maxlength="40"
-                            value="{{ old('form_access_name') }}" aria-describedby="validationform_access_nameFeedback">
-                        <label class="form-label" for="access_name">Nom de l'accès<sup>
-                                <i class="fa-solid fa-asterisk"
-                                    style="font-size: 8px;color: red;margin-top: -14px;"></i></sup></label>
-                        <div class="invalid-feedback">
-                            Le nom de l'accès est obligatoire
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row mt-4">
-        <div class="col">
-            <div class="row">
-                <div class="col d-flex flex-nowrap">
-                    <!--<span class="offer_legend1">4</span>-->
-                    <span class="offer_legend2 align-self-center">Récapitulatif de votre commande<br></span>
-                </div>
-            </div>
-            <div class="row pt-2 offer_content pb-2">
-                <div class="col d-table">
-                    <div class="table-responsive my-3" style="max-width: 50rem;margin-left: auto;margin-right: auto">
-                        <table class="table table-bordered">
-                            <thead>
-                                <tr class="text-center bg-light">
-                                    <th>Formule</th>
-                                    <th>Espace disque</th>
-                                    <th>Total</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr class="text-center">
-                                    <td style="vertical-align:middle">
-                                        <div><span id="recap_level" class="fs-2 fw-bolder">BASIC</span></div>
-                                    </td>
-                                    <td style="vertical-align:middle">
-                                        <div class="d-flex flex-column"><span id="recap_diskspace_go"
-                                                class="fs-2 fw-bold">10 Go</span><span id="recap_diskspace_gio">(
-                                                9.31
-                                                Gio )</span></div>
-                                    </td>
-                                    <td style="vertical-align:middle">
-                                        <div class="d-flex flex-column align-items-start">
-                                            <div id="price_Y" class="ms-5 d-flex flex-wrap"><span id="recap_amount_Y"
-                                                    class="fs-1 text-primary fw-bolder">2
-                                                    €</span><span id="recap_amount_free"
-                                                    class="fs-1 text-primary fw-bolder" style="display: none;">gratuit
-                                                    pour 30 jours</span><span id="recap_duration"
-                                                    class="ms-2 me-1 text-align-buttom d-flex align-items-end">pour
-                                                    1
-                                                    an
-                                                    jusqu'au</span><span id="recap_enddate"
-                                                    class="d-flex align-items-end">01/06/2022</span>
-                                            </div>
-                                            <div id="price_or" class="ms-5" style="display: none;"><span
-                                                    class="fs-3 text-secondary fw-bolder">ou</span></div>
-                                            <div id="price_M" class="ms-5" style="display: none;"><span
-                                                    id="recap_amount_M" class="fs-1 text-primary fw-bolder">2
-                                                    €</span><span class="ms-2">par mois</span></div>
+        <span class="offer_legend2 align-self-center">Récapitulatif de votre commande</span>
+        <div class="offer_content mt-0">
+            <div class="d-table">
+                <div class="table-responsive" style="max-width: 50rem;margin-left: auto;margin-right: auto">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr class="text-center bg-light">
+                                <th>Formule</th>
+                                <th>Espace disque</th>
+                                <th>Total</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="text-center">
+                                <td style="vertical-align:middle">
+                                    <div><span id="recap_level" class="fs-2 fw-bolder">BASIQUE</span></div>
+                                </td>
+                                <td style="vertical-align:middle">
+                                    <div class="d-flex flex-column">
+                                        <span id="recap_diskspace_go" class="fs-2 fw-bold">10 Go</span>
+                                        <span id="recap_diskspace_gio">(9.31 Gio )</span>
+                                    </div>
+                                </td>
+                                <td style="vertical-align:middle">
+                                    <div class="d-flex flex-column align-items-start">
+                                        <div id="price_Y" class="ms-5 d-flex flex-wrap">
+                                            <span id="recap_amount_Y" class="fs-1 text-primary fw-bolder">2€</span>
+                                            <span id="recap_amount_free" lass="fs-1 text-primary fw-bolder" style="display: none;">gratuit pour 30 jours</span>
+                                            <span id="recap_duration" class="ms-2 me-1 text-align-buttom d-flex align-items-end">pour 1 an jusqu'au</span>
+                                            <span id="recap_enddate" class="d-flex align-items-end">01/06/2022</span>
                                         </div>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                                        <div id="price_or" class="ms-5" style="display: none;">
+                                            <span class="fs-3 text-secondary fw-bolder">ou</span>
+                                        </div>
+                                        <div id="price_M" class="ms-5" style="display: none;">
+                                            <span id="recap_amount_M" class="fs-1 text-primary fw-bolder">2€</span>
+                                            <span class="ms-2">par mois</span>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
+        @include('orders.form.formButton')
     </div>
-    <div class="row mt-4">
-        <div class="col">
-            @include('orders.form.formButton')
-        </div>
-    </div>
-    </form>
-    @env('local')
-    <!-- member-access.create -->
-    @endenv
 </div>
+@env('local')
+<!-- member-access.create -->
+@endenv
 @endsection
 
 @push('scripts')
@@ -181,7 +132,7 @@
                 output.innerHTML = slider.value + " Go";
                 percent = Math.round(slider.value / 5000 * 100);
                 slider.style.background = "linear-gradient(to right, #Fe7e20 0%, #Fe7e20 " + percent +
-                    "%, #d5d9da " + percent + "%, #d5d9da 100%)";
+                    "%, #ffd6b8 " + percent + "%, #ffd6b8 100%)";
             })
 
             // Loop over them and prevent submission
