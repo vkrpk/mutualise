e23e071ba9a2f135a1b391a39838c78c18fa561c
vkrpk <victor.krupka@orange.fr>
2022-08-31 16:40:53 +0200

    Envoi d'un email de confirmation à la création d'un compte

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ use TimeHunter\LaravelGoogleReCaptchaV3\Validations\GoogleReCaptchaV3ValidationR
 Route::redirect('/', '/home', 301);
 
 Auth::routes();
+Auth::routes(['verify' => true]);
 Route::post('logout', 'App\Http\Controllers\Auth\LoginController@logout')->name('logout')->withoutMiddleware('2fa');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->withoutMiddleware('auth');
