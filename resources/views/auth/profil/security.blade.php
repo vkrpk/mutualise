d6203107979b9c8ff7b3b282d44463799d422fd2
vkrpk <victor.krupka@orange.fr>
2022-09-01 14:34:09 +0200

    Formulaire de supression de compte dans profil/sécurité

--- resources/views/auth/profil/security.blade.php
+++ resources/views/auth/profil/security.blade.php
@@ -58,24 +58,6 @@
                 <div class="card mb-4">
                     <div class="card-header">Security Preferences</div>
                     <div class="card-body">
-                        <!-- Account privacy optinos-->
-                        <h5 class="mb-1">Account Privacy</h5>
-                        <p class="small text-muted">By setting your account to private, your profile information and posts
-                            will not be visible to users outside of your user groups.</p>
-                        <form>
-                            <div class="form-check">
-                                <input class="form-check-input" id="radioPrivacy1" type="radio" name="radioPrivacy"
-                                    checked="">
-                                <label class="form-check-label" for="radioPrivacy1">Public (posts are available to all
-                                    users)</label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" id="radioPrivacy2" type="radio" name="radioPrivacy">
-                                <label class="form-check-label" for="radioPrivacy2">Private (posts are available to only
-                                    users in your groups)</label>
-                            </div>
-                        </form>
-                        <hr class="my-4">
                         <!-- Data sharing options-->
                         <h5 class="mb-1">Data Sharing</h5>
                         <p class="small text-muted">Sharing usage data can help us to improve our products and better serve
@@ -99,39 +81,15 @@
                 </div>
             </div>
             <div class="col-lg-4">
-                <!-- Two factor authentication card-->
-                {{-- <div class="card mb-4">
-                    <div class="card-header">Two-Factor Authentication</div>
-                    <div class="card-body">
-                        <p>Add another level of security to your account by enabling two-factor authentication. We will send
-                            you a text message to verify your login attempts on unrecognized devices and browsers.</p>
-                        <form>
-                            <div class="form-check">
-                                <input class="form-check-input" id="twoFactorOn" type="radio" name="twoFactor"
-                                    checked="">
-                                <label class="form-check-label" for="twoFactorOn">On</label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" id="twoFactorOff" type="radio" name="twoFactor">
-                                <label class="form-check-label" for="twoFactorOff">Off</label>
-                            </div>
-                            <div class="mt-3">
-                                <label class="small mb-1" for="twoFactorSMS">SMS Number</label>
-                                <input class="form-control" id="twoFactorSMS" type="tel"
-                                    placeholder="Enter a phone number" value="555-123-4567">
-                            </div>
-                        </form>
-                    </div>
-                </div> --}}
                 <x-two-f-a-settings :data="$data"/>
-                <!-- Delete account card-->
                 <div class="card mb-4">
-                    <div class="card-header">Delete Account</div>
+                    <div class="card-header">Supprimer mon compte</div>
                     <div class="card-body">
-                        <p>Deleting your account is a permanent action and cannot be undone. If you are sure you want to
-                            delete your account, select the button below.</p>
-                        <button class="btn btn-danger-soft text-danger" type="button">I understand, delete my
-                            account</button>
+                        <p>Supprimer votre compte est une action permanenete et irréversible. Si vous êtes sur de vouloir supprimer votre compte, sélectionnez le bouton ci-dessous.</p>
+                        <form action="{{ route('removeUserAccount', ['id' => $data['user']->id] )}}" method="POST" onsubmit="return confirm('Êtes-vous sûr de vouloir supprimer votre compte ?');">
+                            @csrf
+                            <button class="btn btn-danger-soft text-danger" type="submit">Je comprends, supprimer mon compte</button>
+                        </form>
                     </div>
                 </div>
             </div>
