c1f8d561f3ec6f5343258566321ef8211e4b597f
vkrpk <victor.krupka@orange.fr>
2022-09-02 08:36:36 +0200

    ajout de la possibilité pour l’user de changer ses informations (qui correspond à la table ‘adresses’) + repopulation du formulaire en cas d’erreur ET dans le cas où l’user a déjà une entrée dans la table ‘addresses’

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,15 @@
 <body>
 
     <x-header />
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+    @elseif (session('error'))
+        <div class="alert alert-danger" role="alert">
+            {{ session('error') }}
+        </div>
+    @endif
     <div>
         @yield('content')
     </div>
