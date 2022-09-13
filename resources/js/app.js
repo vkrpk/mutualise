8625abbe472545641204426fd1b85d3bcd91e63b
vkrpk <victor.krupka@orange.fr>
2022-09-13 11:31:05 +0200

    Import de la police Nunito en local plutot qu'en hot link

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,4 @@
 import "../sass/app.scss";
 import "./bootstrap";
-import.meta.glob(["../images/**"]);
 
-// import "./bootstrap.js";
-// import.meta.glob([
-//     '../images/**',
-//     '../fonts/**',
-// ]);
+import.meta.glob(["../images/**", "../fonts/**"]);
