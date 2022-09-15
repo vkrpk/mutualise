85c2b829e64d570974e57dbf846772e2e313287d
Sukeb <sukebushido@gmail.com>
2022-09-15 17:07:11 +0200

    Traduction de profil/index

--- resources/views/auth/profil/index.blade.php
+++ resources/views/auth/profil/index.blade.php
@@ -13,22 +13,22 @@
             <div class="col-xl-4">
                 <!-- Profile picture card-->
                 <div class="card mb-4 mb-xl-0">
-                    <div class="card-header">Profile Picture</div>
+                    <div class="card-header">{{__('Profile Picture')}}</div>
                     <div class="card-body text-center">
                         <!-- Profile picture image-->
                         <img class="img-account-profile rounded-circle mb-2"
                             src="http://bootdey.com/img/Content/avatar/avatar1.png" alt="">
                         <!-- Profile picture help block-->
-                        <div class="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
+                        <div class="small font-italic text-muted mb-4">{{__("JPG ou PNG inférieur à 5MB")}}</div>
                         <!-- Profile picture upload button-->
-                        <button class="btn btn-primary" type="button">Upload new image</button>
+                        <button class="btn btn-primary" type="button">{{__("Uploader une nouvelle image")}}</button>
                     </div>
                 </div>
             </div>
             <div class="col-xl-8">
                 <!-- Account details card-->
                 <div class="card mb-4">
-                    <div class="card-header">Account Details</div>
+                    <div class="card-header">{{__("Détails du compte")}}</div>
                     <div class="card-body">
                         <form method="POST" action="{{ route('storeInfos') }}">
                             @csrf
@@ -36,7 +36,7 @@
                             <div class="row gx-3 mb-3">
                                 <!-- Form Group (first name)-->
                                 <div class="col-md-6">
-                                    <label class="small mb-1" for="label">Libellé de l'adresse<sup><i class="fa-solid fa-asterisk" style="font-size: 8px;color: red;margin-top: -14px;"></i></sup></label>
+                                    <label class="small mb-1" for="label">{{__("Libellé de l'adresse")}}<sup><i class="fa-solid fa-asterisk" style="font-size: 8px;color: red;margin-top: -14px;"></i></sup></label>
                                     <input class="form-control" id="label" type="text" value="{{ old('label') ? old('label') : $address->label ?? ''  }}"
                                         placeholder="" name="label">
                                     @error('label')
@@ -44,7 +44,7 @@
                                     @enderror
                                 </div>
                                 <div class="col-md-6">
-                                    <label class="small mb-1" for="organization">Organisation</label>
+                                    <label class="small mb-1" for="organization">{{__("Organisation")}}</label>
                                     <input class="form-control" id="organization" type="text"
                                         placeholder="" value="{{ old('organization') ? old('organization') : $address->organization ?? '' }}" name="organization">
                                 </div>
@@ -55,13 +55,13 @@
                             <div class="row gx-3 mb-3">
                                 <!-- Form Group (first name)-->
                                 <div class="col-md-6">
-                                    <label class="small mb-1" for="first_name">Prénom</label>
+                                    <label class="small mb-1" for="first_name">{{__("Prénom")}}</label>
                                     <input class="form-control" id="first_name" type="text"
                                         placeholder="" value="{{ old('first_name') ? old('first_name') : $address->first_name ?? '' }}" name="first_name">
                                 </div>
                                 <!-- Form Group (last name)-->
                                 <div class="col-md-6">
-                                    <label class="small mb-1" for="last_name">Nom</label>
+                                    <label class="small mb-1" for="last_name">{{__("Nom")}}</label>
                                     <input class="form-control" id="last_name" type="text"
                                         placeholder="" value="{{ old('last_name') ? old('last_name') : $address->last_name ?? '' }}" name="last_name">
                                 </div>
@@ -69,7 +69,7 @@
                             <div class="row gx-3 mb-3">
                                 <!-- Form Group (first name)-->
                                 <div class="col-md-6">
-                                    <label class="small mb-1" for="address1">Adresse<sup><i class="fa-solid fa-asterisk" style="font-size: 8px;color: red;margin-top: -14px;"></i></sup></label>
+                                    <label class="small mb-1" for="address1">{{__("Adresse")}}<sup><i class="fa-solid fa-asterisk" style="font-size: 8px;color: red;margin-top: -14px;"></i></sup></label>
                                     <input class="form-control" id="address1" type="text"
                                         placeholder="" value="{{ old('address1') ? old('address1') : $address->address1 ?? '' }}" name="address1">
                                     @error('address1')
@@ -78,7 +78,7 @@
                                 </div>
                                 <!-- Form Group (last name)-->
                                 <div class="col-md-6">
-                                    <label class="small mb-1" for="address2">Complément d'adresse</label>
+                                    <label class="small mb-1" for="address2">{{__("Complément d'adresse")}}</label>
                                     <input class="form-control" id="address2" type="text"
                                         placeholder="" value="{{ old('address2') ? old('address2') : $address->address2 ?? ''}}" name="address2">
                                 </div>
@@ -86,7 +86,7 @@
                             <div class="row gx-3 mb-3">
                                 <!-- Form Group (first name)-->
                                 <div class="col-md-6">
-                                    <label class="small mb-1" for="postal_code">Code postal<sup><i class="fa-solid fa-asterisk" style="font-size: 8px;color: red;margin-top: -14px;"></i></sup></label>
+                                    <label class="small mb-1" for="postal_code">{{__("Code postal")}}<sup><i class="fa-solid fa-asterisk" style="font-size: 8px;color: red;margin-top: -14px;"></i></sup></label>
                                     <input class="form-control" id="postal_code" type="text"
                                         placeholder="" value="{{ old('postal_code') ? old('postal_code') : $address->postal_code ?? '' }}" name="postal_code">
                                     @error('postal_code')
@@ -95,7 +95,7 @@
                                 </div>
                                 <!-- Form Group (last name)-->
                                 <div class="col-md-6">
-                                    <label class="small mb-1" for="city">Ville<sup><i class="fa-solid fa-asterisk" style="font-size: 8px;color: red;margin-top: -14px;"></i></sup></label>
+                                    <label class="small mb-1" for="city">{{__("Ville")}}<sup><i class="fa-solid fa-asterisk" style="font-size: 8px;color: red;margin-top: -14px;"></i></sup></label>
                                     <input class="form-control" id="city" type="text"
                                         placeholder="" value="{{ old('city') ? old('city') : $address->city ?? '' }}" name="city">
                                     @error('city')
@@ -106,13 +106,13 @@
                             <div class="row gx-3 mb-3">
                                 <!-- Form Group (first name)-->
                                 <div class="col-md-6">
-                                    <label class="small mb-1" for="state">État/Province</label>
+                                    <label class="small mb-1" for="state">{{__("État/Province")}}</label>
                                     <input class="form-control" id="state" type="text"
                                         placeholder="" value="{{ old('state') ? old('state') : $address->state ?? '' }}" name="state">
                                 </div>
                                 <!-- Form Group (last name)-->
                                 <div class="col-md-6">
-                                    <label class="small mb-1" for="country">Pays</label>
+                                    <label class="small mb-1" for="country">{{__("Pays")}}</label>
                                     <input class="form-control" id="country" type="text"
                                         placeholder="" value="{{ old('country') ? old('country') : $address->country ?? '' }}" name="country">
                                 </div>
@@ -120,7 +120,7 @@
                             <div class="row gx-3 mb-3">
                                 <!-- Form Group (first name)-->
                                 <div class="col-md-6">
-                                    <label class="small mb-1" for="phone_number">Numéro de téléphone</label>
+                                    <label class="small mb-1" for="phone_number">{{__("Numéro de téléphone")}}</label>
                                     <input class="form-control" id="phone_number" type="text"
                                         placeholder="" value="{{ old('phone_number') ? old('phone_number') : $address->phone_number ?? '' }}" name="phone_number">
                                 </div>
@@ -128,7 +128,7 @@
 
                             </div>
                             <!-- Save changes button-->
-                            <button class="btn btn-primary" type="submit">Sauvegarder</button>
+                            <button class="btn btn-primary" type="submit">{{__("Save")}}</button>
                         </form>
                     </div>
                 </div>
