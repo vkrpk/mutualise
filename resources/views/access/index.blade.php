1a437ef6f45ce7b1ef7e4b08415d2f94ec3635f7
Sukeb <sukebushido@gmail.com>
2022-09-20 08:35:18 +0200

    Trad de access.index.blade.php

--- resources/views/access/index.blade.php
+++ resources/views/access/index.blade.php
@@ -3,11 +3,11 @@
     <div class="container-sm overflow-hidden p-0">
         <div class="row mx-2 mx-sm-0 g-2 text-center m-3">
             <div class="alert alert-primary fs-3 fw-bolder" role="alert">
-                <span>Mes accès</span>
+                <span>{{__("Mes accès")}}</span>
             </div>
         </div>
         <div class="row mx-2 mx-sm-0 g-2 m-3">
-            <a href="{{ route('offers') }}"><button class="btn btn-secondary btn-lg"><i class="fa-solid fa-cloud-arrow-up me-2"></i>Ajouter un accès</button></a>
+            <a href="{{ route('offers') }}"><button class="btn btn-secondary btn-lg"><i class="fa-solid fa-cloud-arrow-up me-2"></i>{{__("Ajouter un accès")}}</button></a>
         </div>
     </div>
 @endsection
