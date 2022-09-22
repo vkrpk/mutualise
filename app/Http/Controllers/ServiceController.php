ef076449af19f27f915a6d981511b996875ee70f
Sukeb <sukebushido@gmail.com>
2022-09-22 09:57:39 +0200

    Ajout de la logique BDD pour valider les changements d'état des services quand l'user est admin

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class ServiceController extends Controller
 {
-    
+    public function serviceUpdate(Request $request, Service $service)
+    {
+        $services = Service::all();
+        foreach ($services as $service) {
+            $service->update(['is_active' => false]);
+        }
+        if(isset($request->service) ){
+            foreach ($request->service as $inputService => $value) {
+                if ($value == 'on') {
+                    $service = Service::where('name', $inputService)
+                        ->update(['is_active' => true]);
+                }
+            }
+        }
+
+        return back()->with('status', 'Les changements ont bien été pris en compte');
+    }
 }
