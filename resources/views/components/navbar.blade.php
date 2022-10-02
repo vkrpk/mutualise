34eb5088ae6fd49eebea58163df3560d4264e837
vkrpk <victor.krupka@orange.fr>
2022-10-02 14:53:26 +0200

    remove no using links

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,7 +3,7 @@
         $linkStyle = "mx-2 mx-sm-3";
     @endphp
     <a class="nav-link {{$linkStyle}} {{ $route === 'profilIndex' ? "active" : ""}}" href="{{ route('profilIndex') }}">{{__('Profile')}}</a>
-    <a class="nav-link {{$linkStyle}} {{ $route === 'profilBilling' ? "active" : ""}}" href="{{ route('profilBilling') }}">{{__('Paiement')}}</a>
+    {{-- <a class="nav-link {{$linkStyle}} {{ $route === 'profilBilling' ? "active" : ""}}" href="{{ route('profilBilling') }}">{{__('Paiement')}}</a> --}}
     <a class="nav-link {{$linkStyle}} {{ $route === 'profilSecurity' ? "active" : ""}}" href="{{ route('profilSecurity') }}">{{__('Sécurité')}}</a>
-    <a class="nav-link {{$linkStyle}} {{ $route === 'profilNotifications' ? "active" : ""}}" href="{{ route('profilNotifications') }}">Notifications</a>
+    {{-- <a class="nav-link {{$linkStyle}} {{ $route === 'profilNotifications' ? "active" : ""}}" href="{{ route('profilNotifications') }}">Notifications</a> --}}
 </nav>
