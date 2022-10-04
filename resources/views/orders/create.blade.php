2374508a073392f4ead01637fd289ce56aecbdac
Sukeb <sukebushido@gmail.com>
2022-10-04 11:14:28 +0200

    Traduction de create.blade.php

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -12,7 +12,7 @@
             <div class="col">
                 <div class="card mb-4">
                     <div class="card-header customCardHeader">
-                        <span>Adresse de facturation</span>
+                        <span>{{__("Adresse de facturation")}}</span>
                     </div>
                     <div class="card-body">
                         <x-adresses-card :address="$address" :form="'formRecapOrder'"/>
@@ -22,38 +22,34 @@
             <div class="col">
                 <div class="card mb-4">
                     <div class="card-header customCardHeader">
-                        <span>Récapitulatif de l'offre</span>
+                        <span>{{__("Récapitulatif de l'offre")}}</span>
                     </div>
                     <div class="card-body">
                         <div class="row justify-content-evenly">
                             <div class="row">
                                 <div class="col-12 col-sm-6 ps-1 pe-0">
                                     <span class="fst-italic">
-                                        <i class="fa-solid fa-circle-arrow-right bg-white text-secondary me-2"></i>Formule
-                                        choisie :</span>
+                                        <i class="fa-solid fa-circle-arrow-right bg-white text-secondary me-2"></i>{{__("Formule choisie")}} :</span>
                                     <span>{{ ucfirst($item->attributes->form_level) }}</span>
                                 </div>
                                 @if ($item->attributes->buttonsRadioForOffer)
                                     <div class="col-12 col-sm-6 ps-1 pe-0">
                                         <span class="fst-italic"><i
-                                                class="fa-solid fa-circle-arrow-right bg-white text-secondary me-2"></i>Option
-                                            :</span>
+                                                class="fa-solid fa-circle-arrow-right bg-white text-secondary me-2"></i>Option :</span>
                                         <span>{{ $buttonsRadioForOfferName }}</span>
                                     </div>
                                 @endif                                
                                 @if ($item->attributes->isFreeTrial == true)
                                     <div class="col-12 col-sm-6 ps-1 pe-0">
                                         <span class="fst-italic"><i
-                                                class="fa-solid fa-circle-arrow-right bg-white text-secondary me-2"></i>Option
-                                            :</span>
-                                        <span>Offre d'essai</span>
+                                                class="fa-solid fa-circle-arrow-right bg-white text-secondary me-2"></i>Option :</span>
+                                        <span>{{_("Offre d'essai")}}</span>
                                     </div>
                                 @endif
                             </div>
                             <div class="row">
                                 <div class="col ps-1 pe-0">
-                                    <span class="fst-italic"><i class="fa-solid fa-circle-arrow-right bg-white text-secondary me-2"></i>Espace
-                                        disque :</span>
+                                    <span class="fst-italic"><i class="fa-solid fa-circle-arrow-right bg-white text-secondary me-2"></i>{{__("Espace disque")}} :</span>
                                     <span>{{ $item->attributes->form_diskspace . ' Go' }}</span>
                                 </div>
                             </div>
@@ -63,7 +59,7 @@
                                     <div class="col ps-1 pe-0 d-flex align-item-center">
                                         <span class="d-flex align-items-center"><i class="fa-solid fa-circle-arrow-right bg-white text-secondary me-2"></i></span>
                                         <div>
-                                            <span class="fst-italic">Domaine :</span>
+                                            <span class="fst-italic">{{_("Domaine")}} :</span>
                                             <span>{{ $item->attributes->domainUrlOrPrefix }}</span>
                                         </div>
                                     </div>
@@ -73,28 +69,28 @@
                     </div>
                     <div class="card-footer d-flex justify-content-between">
                         <span>Total : </span>
-                        <span>{{ $formula_period === 'yearly' ? $item->price : $item->attributes->priceMonthly }} € {{ $formula_period === "monthly" ? "par mois" : ($formula_period === "free" ? "pour un mois" : "pour un an") }}</span>
+                        <span>{{ $formula_period === 'yearly' ? $item->price : $item->attributes->priceMonthly }} € {{ $formula_period === "monthly" ? __("par mois") : ($formula_period === "free" ? __("pour un mois") : __("pour un an")) }}</span>
                     </div>
                 </div>
                 <div class="card mb-4">
                     <div class="card-header customCardHeader">
-                        <span>Conditions de vente</span>
+                        <span>{{__("Conditions de vente")}}</span>
                     </div>
                     <div class="card-body">
-                        <span class="d-block fw-bolder">Identité de l'association : Dedikam</span>
+                        <span class="d-block fw-bolder">{{__("Identité de l'association : Dedikam")}}</span>
                         <span class="d-block"><strong>Raison sociale :</strong> Association régie par la loi du 1er juillet 1901 et le décret du 16 août 1901. TVA non applicable art-293B du CGI.</span>
-                        <span class="d-block"><strong>Support technique :</strong><a class="btn-link" href="mailto:support@dedikam.com"> support@dedikam.com</a></span>
+                        <span class="d-block"><strong>{{__("Support technique")}} :</strong><a class="btn-link" href="mailto:support@dedikam.com"> support@dedikam.com</a></span>
                         <span class="d-block">Identifiant SIREN : 503341976</span>
                         <span class="d-block">CNIL : 1426354</span>
                         <span class="d-block">Code APE : 9499Z</span>
                         <span class="d-block">Identification WALDEC : W763003888</span>
-                        <a href="https://labo-drupal.dedikam.com/content/conditions-de-vente" class="text-primary">Lire la suite</a>
+                        <a href="https://labo-drupal.dedikam.com/content/conditions-de-vente" class="text-primary">{{__("En savoir plus")}}</a>
                     </div>
                     <div class="card-footer">
                         <input class="form-check-input" type="checkbox" id="checboxCGU" name="checboxCGU" form="formRecapOrder">
-                        <label class="form-check-label" for="checboxCGU"> J'accepte les conditions ci-dessus </label><br>
+                        <label class="form-check-label" for="checboxCGU"> {{__("J'accepte les conditions ci-dessus")}} </label><br>
                         @error('checboxCGU')
-                            <span class="ms-3 text-danger">Vous devez accepter les conditions de vente pour continuer.</span>
+                            <span class="ms-3 text-danger">{{__("Vous devez accepter les conditions de vente pour continuer.")}}</span>
                         @enderror
                     </div>
                 </div>
@@ -104,14 +100,14 @@
             <div class="col">
                 <div class="card">
                     <div class="card-header customCardHeader">
-                        <span>Commentaires concernant votre commande</span>
+                        <span>{{__("Commentaires concernant votre commande")}}</span>
                     </div>
                     <div class="card-body">
-                        <p>Utilisez cette zone pour des instructions spéciales ou des questions concernant votre commande.
+                        <p>{{__("Utilisez cette zone pour des instructions spéciales ou des questions concernant votre commande.")}}
                         </p>
                         <div class="form-floating">
                             <textarea class="form-control" id="comment" style="height: 6rem;" form="formRecapOrder" name="comment"></textarea>
-                            <label class="form-label" for="comment">Commentaires concernant la commande</label>
+                            <label class="form-label" for="comment">{{__("Commentaires concernant votre commande")}}</label>
                         </div>
                     </div>
                 </div>
@@ -123,7 +119,7 @@
                     @csrf
                     <input type="hidden" value="{{$cartItemId}}" name="cartItemId">
                     <input type="hidden" value="{{$formula_period}}" name="formula_period">
-                    <button type="submit" class="btn btn-primary btn-lg" id="buttonFormRecapOrder">Valider la commande</button>
+                    <button type="submit" class="btn btn-primary btn-lg" id="buttonFormRecapOrder">{{__("Valider la commande")}}</button>
                 </form>
             </div>
         </div>
