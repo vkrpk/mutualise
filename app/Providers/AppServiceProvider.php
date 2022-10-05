fe3cae06a9c191cd76123bfa3748eb7af5fc5239
vkrpk <victor.krupka@orange.fr>
2022-10-05 09:10:34 +0200

    modif url

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \Debugbar::enable();
         Paginator::useBootstrap();
 
         Blade::directive('isAdmin', function() {
