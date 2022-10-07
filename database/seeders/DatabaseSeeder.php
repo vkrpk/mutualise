cc1c0e0a3b01bbad0aafef070478dcbc0559bf8d
vkrpk <victor.krupka@orange.fr>
2022-10-07 09:20:21 +0200

    change filesystems default url in public

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             CouponSeeder::class,
             ServiceSeeder::class,
             FormulaSeeder::class,
+            VoyagerDatabaseSeeder::class
         ]);
     }
 }
