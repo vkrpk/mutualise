97d5dfcd43d0c0aad2589df354ce01e55a6e7d2f
vkrpk <victor.krupka@orange.fr>
2023-12-28 11:46:28 +0100

    :construction: Refonte style

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer-basic mt-auto py-1 bg-info fixed-bottom position-relative">
-    <ul class="list-inline mt-md-4 text-primary">
+    <ul class="list-inline text-secondary">
         <li class="list-inline-item"><a href="{{ route('contact.index') }}">Contact</a></li>
         <li class="list-inline-item"><a href="{{ route('footer.cguv') }}">{{__("CGUV")}}</a></li>
         <li class="list-inline-item"><a href="{{ route('footer.politique-confidentialite') }}">{{__("Politique de confidentialité")}}</a></li>
@@ -8,7 +8,7 @@
             <li class="list-inline-item social pb-0"><a href="https://twitter.com/DediKam"><i class="fa-brands fa-twitter"></i></a></li>
         </div>
     </ul>
-    <p class="copyright text-primary">Dedikam © 2008 - 2021</p>
+    <p class="copyright text-primary">Victork © {{ now()->year }}</p>
     @isAdmin()
         <div class="admin rounded-2 text-white">Administrateur</div>
     @endisAdmin
